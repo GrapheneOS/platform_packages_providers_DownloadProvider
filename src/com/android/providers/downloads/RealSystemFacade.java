0671bead23b6582536c0e924fffd018af42b5c9d
Sandro Montanari <sandrom@google.com>
2025-10-10 05:37:38 -0700

    Refactor getApplicationConfigForPackage method
    Following API reviews from ag/35824240, I am moving the
    getApplicationConfigForPackage method from the NetworkSecurityPolicy
    class to the ApplicationConfig class, and renaming the method from "get"
    to "create".
    
    Bug: 397646538
    Test: atest NetworkSecurityConfigTests
    Flag: build.release_conscrypt_nsc
    Change-Id: Ic25c7f11bb2f5a28fd2b687d53ca4d213a73a2ee

--- src/com/android/providers/downloads/RealSystemFacade.java
+++ src/com/android/providers/downloads/RealSystemFacade.java
@@ -20,20 +20,13 @@ import android.app.DownloadManager;
 import android.app.job.JobParameters;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.security.NetworkSecurityPolicy;
 import android.security.net.config.ApplicationConfig;
 import android.security.net.config.ConfigNetworkSecurityPolicy;
-
-import com.android.internal.util.ArrayUtils;
 
 import java.security.GeneralSecurityException;
 
@@ -95,7 +88,7 @@ class RealSystemFacade implements SystemFacade {
             throws GeneralSecurityException {
         ApplicationConfig appConfig;
         try {
-            appConfig = NetworkSecurityPolicy.getApplicationConfigForPackage(context, packageName);
+            appConfig = ApplicationConfig.createApplicationConfigForPackage(context, packageName);
         } catch (NameNotFoundException e) {
             // Unknown package -- fallback to the default SSLContext
             return SSLContext.getDefault();
@@ -116,7 +109,7 @@ class RealSystemFacade implements SystemFacade {
     public boolean isCleartextTrafficPermitted(String packageName, String host) {
         ApplicationConfig appConfig;
         try {
-            appConfig = NetworkSecurityPolicy.getApplicationConfigForPackage(mContext, packageName);
+            appConfig = ApplicationConfig.createApplicationConfigForPackage(mContext, packageName);
         } catch (NameNotFoundException e) {
             // Unknown package -- fail for safety
             return false;
@@ -133,7 +126,7 @@ class RealSystemFacade implements SystemFacade {
     @Override
     public boolean hasPerDomainConfig(String packageName) {
         try {
-            return NetworkSecurityPolicy.getApplicationConfigForPackage(mContext,
+            return ApplicationConfig.createApplicationConfigForPackage(mContext,
                     packageName).hasPerDomainConfigs();
         } catch (NameNotFoundException e) {
             // Unknown package -- fail for safety
